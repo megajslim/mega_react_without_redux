86e3a8a776cb628bc27953f8f7f59d74b3e88d98
megajslim <jslim@megastudy.net>
2021-06-10 12:19:17 +0900

    환경 설정

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8020/api",
+  baseURL: `${process.env.REACT_APP_SERVER_BASE_URL}/api`,
   headers: {
     "Content-type": "application/json",
     "ApiKey" : "test1234"
